940094d22cf853e9b51d72fea83aabc31d48dfda
GswapG <swappyontube@gmail.com>
2024-04-08 09:56:51 +0530

    First pass completed

--- assembler.cpp
+++ assembler.cpp
@@ -4,8 +4,11 @@
 #include <sstream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <iomanip>
 #include <algorithm>
+
+std::unordered_set<std::string> assemblerDirective;
 
 std::vector<std::vector<std::string>> readInputFile(const std::string& filename) {
     std::vector<std::vector<std::string>> result;
@@ -88,7 +91,7 @@ void printAssembly(std::vector<std::vector<std::string>> &instructions){
                 default:
                     break;
             }
-            std::cout << std::setw(8) << row[i];
+            std::cout << std::setw(11) << row[i];
             // Reset color
             std::cout << "\033[0m";
         }
@@ -128,6 +131,39 @@ std::unordered_map<std::string, std::pair<int, bool>> importSymbolTable(const st
     return symbolTable;
 }
 
+void importAssemblerDirectives(std::string filename){
+    std::ifstream file(filename);
+    // Check if the file exists and can be opened
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return; 
+    }
+    std::string line;
+    while(std::getline(file,line)){
+        assemblerDirective.insert(line);
+    }
+    file.close();
+}
+void storeSymbolTable(const std::string& filename, const std::unordered_map<std::string, std::pair<int, bool>>& symbolTable) {
+    // Open the CSV file for writing
+    std::ofstream outputFile(filename);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Unable to create CSV file" << std::endl;
+        return;
+    }
+
+    // Write column headers
+    outputFile << "Label, Location, Error Flag" << std::endl;
+
+    // Write data from the symbol table
+    for (const auto& entry : symbolTable) {
+        outputFile << entry.first << ", " << entry.second.first << ", " << std::boolalpha << entry.second.second << std::endl;
+    }
+
+    // Close the CSV file
+    outputFile.close();
+}
+
 void firstPass(const std::vector<std::vector<std::string>>& instructions, const std::unordered_map<std::string, std::pair<std::string, int>>& opcodeTable, std::unordered_map<std::string, std::pair<int, bool>>& symbolTable) {
     int locationCounter = 0;
     
@@ -140,31 +176,57 @@ void firstPass(const std::vector<std::vector<std::string>>& instructions, const 
 
     for (const auto& instruction : instructions) {
         // Write location counter and instruction to the intermediate file
-        intermediateFile << std::setw(4) << std::hex << locationCounter << " "; // Set width to 4 characters for hexadecimal output
+        intermediateFile << std::setw(6) << std::hex << locationCounter << " "; // Set width to 4 characters for hexadecimal output
         if(!instruction[0].empty()){
-            intermediateFile << instruction[0];
-            intermediateFile << " ";
+            intermediateFile << std::setw(7) << instruction[0];
         }
         else{
-            intermediateFile << "     ";
+            intermediateFile << "       ";
         }
         //opcode check
         if(instruction[1][0] == '+'){
             // Write '+' character for instructions starting with '+'
-            intermediateFile << instruction[1];
+            intermediateFile << std::setw(7) << instruction[1];
             // Check opcode table without '+'
             if (opcodeTable.find(instruction[1].substr(1)) != opcodeTable.end()) {
                 locationCounter += 4;
-            } else {
+            }
+            else{
                 std::cerr << "Error: Opcode not found for mnemonic '" << instruction[1] << "'" << std::endl;
             }
+                
+            
         }
         else{
-            intermediateFile << instruction[1];
+            intermediateFile << std::setw(7) << instruction[1];
             // Check opcode table normally
             if (opcodeTable.find(instruction[1]) != opcodeTable.end()) {
                 locationCounter += opcodeTable.at(instruction[1]).second;
-            } else {
+            } 
+            else if(assemblerDirective.find(instruction[1]) != assemblerDirective.end()){
+                // assembler directive
+                if(*assemblerDirective.find(instruction[1]) == "BYTE"){
+                    // according to length of third colm
+                    if(instruction[2][0] == 'X'){
+                        // hex values
+                        locationCounter += (instruction[2].length()-3)/2; //POTENTIAL ERROR
+                    }
+                    else if(instruction[2][0] == 'C'){
+                        // string values
+                        locationCounter += (instruction[2].length()-3);
+                    }
+                }
+                else if(*assemblerDirective.find(instruction[1]) == "WORD"){
+                    locationCounter += 3;
+                }
+                else if(*assemblerDirective.find(instruction[1]) == "RESW"){
+                    locationCounter += 3*stoi(instruction[2]);
+                }
+                else if(*assemblerDirective.find(instruction[1]) == "RESB"){
+                    locationCounter += stoi(instruction[2]);
+                }
+            }
+            else {
                 std::cerr << "Error: Opcode not found for mnemonic '" << instruction[1] << "'" << std::endl;
             }
         }
@@ -189,12 +251,17 @@ void firstPass(const std::vector<std::vector<std::string>>& instructions, const 
 
     // Close the intermediate file
     intermediateFile.close();
+    storeSymbolTable("symboltable.txt",symbolTable);
 }
 
 int main(){
     std::unordered_map<std::string, std::pair<std::string,int>> OpcodeTable;
     std::unordered_map<std::string, std::pair<int,bool>> SymbolTable;
     OpcodeTable = importOpcodeTable("opcodes.txt");
+    importAssemblerDirectives("directives.txt");
+    // for(auto &i : assemblerDirective){
+    //     std::cout << i << " ";
+    // }
     std::string filename;
     std::cout<<"input file name to read : ";
     std::cin>>filename; 
